130b527272bd0c094a88ce55506b199fd8c34fcf
Skallad <gramatikofski.elena@gmail.com>
2022-09-24 20:51:05 +0000

    Add template for poem and make basic functions for mad lib

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Modules for styling
+from art import *
+from rich import *
+
+
+# Mad lib reader template
+class MadLibReader:
+    def __init__(self, word_inputs, template):
+        self.poem = poem
+        self.word_inputs = word_inputs
+
+
+# Welcome message
+tprint("")
+
+
+def get_user_input(word_inputs):
+    words = []
+    print("Please provide a few words for the text")
+    for desc in word_inputs:
+        user_input = input(desc + " ")
+        words.append(user_input)
+    return words
+
+
+# Build the poem
+def assemble_poem(template, words):
+    template = "I love you, my {} {}!"
+    words = get_user_input(["adjective", "noun"])
+    poem = template.format(*words)
+    return poem
+
+
